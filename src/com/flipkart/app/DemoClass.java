648999fc2a4df91b2d4e68f8863916d8710d35f9
manav-bhagchandani <manav.bhagchandani@flipkart.com>
2023-01-24 14:27:09 +0530

    first push

---
+++ src/com/flipkart/app/DemoClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author manav.bhagchandani
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
